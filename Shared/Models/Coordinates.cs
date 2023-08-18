2e172b8457b89afa2d88b60805b8110cfc87dcf4
EducatedStrikeCart <dhn004@gmail.com>
2023-08-17 22:50:42 -0400

    DTOs

---
+++ Shared/Models/Coordinates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Shared.Models
+{
+	public record Coordinates
+	{
+		public double Latitude { get; init; }
+		public double Longitude { get; init; }
+
+		public Coordinates(double latitude, double longitude)
+		{
+			Latitude = latitude;
+			Longitude = longitude;
+		}
+	}
+}
--- Shared/Models/GeocoderResult.cs
+++ Shared/Models/GeocoderResult.cs
@@ -1,17 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace WeatherApp.Shared.Models
 {
-	public record GeocoderResult
+	public class GeocoderResult
 	{
+		[JsonPropertyName("name")]
 		public string Name;
+
+		[JsonPropertyName("local_name")]
 		public string[] LocalNames;
+
+		[JsonPropertyName("lat")]
 		public double Latitude;
+
+		[JsonPropertyName("lon")]
 		public double Longitude;
+
+		[JsonPropertyName("country")]
 		public string CountryCode;
+
+		public GeocoderResult(string name, string[] localNames, double latitude, double longitude, string countryCode)
+		{
+			Name = name;
+			LocalNames = localNames;
+			Latitude = latitude;
+			Longitude = longitude;
+			CountryCode = countryCode;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return Equals(obj as GeocoderResult);
+		}
+
+		public bool Equals(GeocoderResult? other)
+		{
+			return other is not null &&
+				   Name == other.Name &&
+				   EqualityComparer<string[]>.Default.Equals(LocalNames, other.LocalNames) &&
+				   Latitude == other.Latitude &&
+				   Longitude == other.Longitude &&
+				   CountryCode == other.CountryCode;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Name, LocalNames, Latitude, Longitude, CountryCode);
+		}
+
+
 	}
 }
